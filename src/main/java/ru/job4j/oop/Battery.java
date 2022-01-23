5a121fabf246bf418ac60b3b50b60a4cb3a0c06d
khlap <khlaponin.dp@gmail.com>
2022-01-23 15:28:20 +0100

    1.8. Состояние объекта [#167388 #254511]

---
+++ src/main/java/ru/job4j/oop/Battery.java
@@ -0,0 +1,22 @@
+package ru.job4j.oop;
+
+public class Battery {
+    private int load;
+
+    public Battery() {
+    }
+
+    public Battery(int load) {
+        this.load = load;
+    }
+
+    public int getLoad() {
+        return load;
+    }
+
+    public void exchange(Battery another) {
+        this.load = this.load  + another.load;
+        another.load = 0;
+
+    }
+}
