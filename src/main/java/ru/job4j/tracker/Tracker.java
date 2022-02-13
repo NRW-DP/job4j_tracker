62311520eef9c826f904c2d867b6a26260d01c40
khlap <khlaponin.dp@gmail.com>
2022-02-13 12:55:38 +0100

    Tracker - хранилище [#396 #263893]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -23,19 +25,19 @@ public class Tracker {
         return rsl;
     }
 
-//    public Item[] findAll() {
-//       return
-//    }
+    public Item[] findAll() {
+       return Arrays.copyOf(items, size);
+    }
 
     public Item[] findByName(String key) {
-            Item[] found = new Item[items.length];
+            Item[] found = new Item[size];
             int count = 0;
-        for (int i = 0; i < items.length - 1; i++) {
+        for (int i = 0; i < size; i++) {
             if (items[i].getName().equals(key)) {
-                found[0] = items[i];
+                found[count] = items[i];
                 count++;
             }
         }
-        return found;
+        return Arrays.copyOf(found, count);
     }
         }
