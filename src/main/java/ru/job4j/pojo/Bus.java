c95abe09cbb7aa37dc73347ff3ed3ea828c33215
khlap <khlaponin.dp@gmail.com>
2022-02-22 21:17:39 +0100

    3. Интерфейс Input [#181092]

---
+++ src/main/java/ru/job4j/pojo/Bus.java
@@ -0,0 +1,16 @@
+package ru.job4j.pojo;
+
+public class Bus implements Transport {
+    @Override
+    public void drive() {
+    }
+
+    @Override
+    public void passenger(int count) {
+    }
+
+    @Override
+    public double refuel(double quantity, double price) {
+        return quantity / price;
+    }
+}
---
+++ src/main/java/ru/job4j/pojo/Transport.java
@@ -0,0 +1,9 @@
+package ru.job4j.pojo;
+
+public interface Transport {
+    void drive();
+
+    void passenger(int count);
+
+    double refuel(double quantity, double price);
+}
