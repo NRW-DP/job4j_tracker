645aac6a6005d4adc1b94f33efcdb9a95a6de09b
khlap <khlaponin.dp@gmail.com>
2022-02-23 19:21:12 +0100

    3. Интерфейс Input [#181092 #268351]

--- src/main/java/ru/job4j/pojo/Bus.java
+++ src/main/java/ru/job4j/pojo/Bus.java
@@ -3,14 +3,17 @@ package ru.job4j.pojo;
 public class Bus implements Transport {
     @Override
     public void drive() {
+        System.out.println("Поехали");
     }
 
     @Override
     public void passenger(int count) {
+        System.out.println("Количество пассажиров в поездке: " + count);
     }
 
      @Override
     public double refuel(double quantity, double price) {
+        price *= quantity;
         return price;
     }
 }
