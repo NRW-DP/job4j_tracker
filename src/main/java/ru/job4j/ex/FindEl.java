b121393789abb68a3c948b50e1ba83ef1a37ae3a
khlap <khlaponin.dp@gmail.com>
2022-03-19 18:50:01 +0100

    Пользовательские исключения. [#219367]

---
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -0,0 +1,26 @@
+package ru.job4j.ex;
+
+public class FindEl {
+    public static int indexOf(String[] value, String key) throws ElementNotFoundException {
+        int rsl = -1;
+        for (int i = 0; i < value.length; i++) {
+            if (value[i].equals(key)) {
+                rsl = i;
+                break;
+            }
+        }
+        if (rsl == -1) {
+            throw new ElementNotFoundException("Element \"" + key + "\" is not found");
+        }
+        return rsl;
+    }
+
+    public static void main(String[] args) {
+        String[] strings = {"123", "456"};
+        try {
+            indexOf(strings, "789");
+        } catch (ElementNotFoundException e) {
+            e.printStackTrace();
+        }
+    }
+}
