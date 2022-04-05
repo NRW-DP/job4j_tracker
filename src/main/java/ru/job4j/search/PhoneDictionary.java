ff93b76f1bce687c18b0a7d03d93b6c6f4627ba8
khlap <khlaponin.dp@gmail.com>
2022-04-05 18:26:12 +0200

    Телефонный справочник на базе ArrayList [#41598]

---
+++ src/main/java/ru/job4j/search/PhoneDictionary.java
@@ -0,0 +1,21 @@
+package ru.job4j.search;
+
+import java.util.ArrayList;
+
+public class PhoneDictionary {
+    private ArrayList<Person> persons = new ArrayList<>();
+
+    public void add(Person person) {
+        this.persons.add(person);
+    }
+
+    public ArrayList<Person> find(String key) {
+        ArrayList<Person> result = new ArrayList<>();
+        for (Person element : persons) {
+            if (element.getPhone().contains(key) || element.getAddress().contains(key) || element.getName().contains(key) || element.getSurname().contains(key)) {
+                result.add(element);
+            }
+        }
+        return result;
+    }
+}
